b0b990ddcbc1bbcf1c5ec8193ea886cefcfe9e44
Bobby Galli <bobbyg603@gmail.com>
2021-04-25 09:36:33 -0400

    Use ICollection for AdditionalFormDataParams because it supports Add.

--- BugSplatDotNetStandard/BugSplatPostOptions.cs
+++ BugSplatDotNetStandard/BugSplatPostOptions.cs
@@ -5,7 +5,7 @@ namespace BugSplatDotNetStandard
 {
     public class BugSplatPostOptions
     {
-        public IEnumerable<KeyValuePair<string, HttpContent>> AdditionalFormDataParams { get; } = new List<KeyValuePair<string, HttpContent>>();
+        public ICollection<KeyValuePair<string, HttpContent>> AdditionalFormDataParams { get; } = new List<KeyValuePair<string, HttpContent>>();
         public string Description { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Key { get; set; } = string.Empty;
