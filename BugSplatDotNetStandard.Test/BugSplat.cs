c21fa158128f575bc91a5a18f82a4b683c22b26c
Bobby Galli <bobbyg603@gmail.com>
2021-04-26 10:42:41 -0400

    Add support for posting minidumps (#4)
    * Add support for posting minidumps
    Add support for post params and overrides
    Add XML documentation comments
    Fixes #1
    Fixes #2
    Fixes #3
    
    * Use ICollection for AdditionalFormDataParams because it supports Add.
    
    * Improved attachment support. (#7)
    
    * Improved attachment support.
    Fixes #6
    
    * Improved property access.
    
    * Fix Sharing Violation Exception reading attachment bytes.
    Fixes #8

--- BugSplatDotNetStandard.Test/BugSplat.cs
+++ BugSplatDotNetStandard.Test/BugSplat.cs
@@ -1,6 +1,7 @@
 using BugSplatDotNetStandard;
 using NUnit.Framework;
 using System;
+using System.IO;
 using System.Net;
 
 namespace Tests
@@ -13,17 +14,82 @@ namespace Tests
         {
             try
             {
-                throw new Exception("BugSplat!");
+                new Foo(new Bar(new Baz())).SampleStackFrame();
             }
             catch (Exception ex)
             {
-                var sut = new BugSplat("octomore", "MyUwpCrasherTest", "1.0.0.0");
+                var sut = new BugSplat("fred", "MyDotNetStandardCrasher", "1.0");
 
-                var response = sut.Post(ex).Result;
+                var options = new BugSplatPostOptions()
+                {
+                    Description = "BugSplat rocks!",
+                    Email = "fred@bugsplat.com",
+                    User = "Fred",
+                    Key = "the key!"
+                };
+                var response = sut.Post(ex, options).Result;
                 var body = response.Content.ReadAsStringAsync().Result;
 
                 Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             }
+        }
+
+        [Test]
+        public void BugSplat_PostMinidump_ShouldPostMinidumpToBugSplat()
+        {
+            var sut = new BugSplat("fred", "myConsoleCrasher", "2021.4.23.0");
+
+            var minidumpFileInfo = new FileInfo("minidump.dmp");
+            // TODO BG backend support for these properties!
+            var options = new BugSplatPostOptions()
+            {
+                Description = "BugSplat rocks!",
+                Email = "fred@bugsplat.com",
+                User = "Fred",
+                Key = "the key!"
+            };
+            var response = sut.Post(minidumpFileInfo, options).Result;
+            var body = response.Content.ReadAsStringAsync().Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+
+    public class Foo
+    {
+        private Bar _bar;
+
+        public Foo(Bar bar)
+        {
+            _bar = bar;
+        }
+
+        public void SampleStackFrame()
+        {
+            _bar.SampleStackFrame();
+        }
+    }
+
+    public class Bar
+    {
+        private Baz _baz;
+
+        public Bar(Baz baz)
+        {
+            _baz = baz;
+        }
+
+        public void SampleStackFrame()
+        {
+            _baz.SampleStackFrame();
+        }
+    }
+
+    public class Baz
+    {
+        public void SampleStackFrame()
+        {
+            throw new Exception("BugSplat!");
         }
     }
 }
---
+++ BugSplatDotNetStandard/BugSplatPostOptions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace BugSplatDotNetStandard
+{
+    public class BugSplatPostOptions
+    {
+        /// <summary>
+        /// A list of attachments to be added to the post
+        /// </summary>
+        public List<FileInfo> AdditionalAttachments { get; } = new List<FileInfo>();
+
+        /// <summary>
+        /// A list of form data key value pairs to be added to the post
+        /// </summary>
+        public List<KeyValuePair<string, HttpContent>> AdditionalFormDataParams { get; } = new List<KeyValuePair<string, HttpContent>>();
+        
+        /// <summary>
+        /// A description to be added to the post that overrides the corresponding default value
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// An email to be added to the post that overrides the corresponding default value
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// A key to be added to the post that overrides the corresponding default value
+        /// </summary>
+        public string Key { get; set; } = string.Empty;
+
+        /// <summary>
+        /// An user to be added to the post that overrides the corresponding default value
+        /// </summary>
+        public string User { get; set; } = string.Empty;
+
+    }
+}
