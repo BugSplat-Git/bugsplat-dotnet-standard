e34fd48b41fae6f6729ec60c33199506a697cb27
Bobby Galli <bobbyg603@gmail.com>
2021-11-24 17:11:01 -0500

    Replaces Newtonsoft with homebrewed json parser
    Fixes #39

---
+++ BugSplatDotNetStandard.Test/JsonObject.cs
@@ -0,0 +1,33 @@
+﻿using BugSplatDotNetStandard.Utils;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class JsonObjectTest
+    {
+
+        [Test]
+        public void JsonObject_GetValue_ShouldReturnValueForTopLevelKey()
+        {
+            var expected = "https://bugsplat.com";
+            var json = $@"{{ ""url"": ""{ expected }"" }}";
+            var obj = new JsonObject(json);
+
+            var result = obj.GetValue("url");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void JsonObject_GetValue_ShouldReturnValueForNestedKey()
+        {
+            var expected = "rocks!";
+            var json = $@"{{ ""bug"": {{  ""splat"": ""{expected}"" }} }}";
+            var obj = new JsonObject(json);
+
+            var result = obj.GetValue("bug", "splat");
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ BugSplatDotNetStandard/JsonObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+[assembly: InternalsVisibleTo("BugSplatDotNetStandard.Test")]
+namespace BugSplatDotNetStandard.Utils
+{
+    internal class JsonObject
+    {
+        private string _json;
+
+        public JsonObject(string json)
+        {
+            _json = json;
+        }
+
+        public string GetValue(params string[] path)
+        {
+            var jsonBytes = Encoding.UTF8.GetBytes(_json);
+            var quotas = new XmlDictionaryReaderQuotas();
+            var jsonReader = JsonReaderWriterFactory.CreateJsonReader(jsonBytes, quotas);
+            var root = XElement.Load(jsonReader);
+            var key = string.Join("/", path);
+            return root.XPathSelectElement($"//{key}").Value;
+        }
+    }
+}
